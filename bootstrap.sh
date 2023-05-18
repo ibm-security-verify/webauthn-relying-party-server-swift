22be90cc609631bb1ab5044e4b0ea4793ae110f8
craigaps <craigaps@au1.ibm.com>
2023-05-18 10:13:36 +1000

    Update bootstrap.sh
    Remove debugging.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 # Copyright contributors to the IBM Security Verify WebAuthn Relying Party Server for Swift
 
-/app/RelyingPartyServer serve --env development --hostname 0.0.0.0 --port 8080 --log debug
+/app/RelyingPartyServer serve --env production --hostname 0.0.0.0 --port 8080
